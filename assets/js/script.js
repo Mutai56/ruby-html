e627b25a39ad44798abe94660aeed34b2a512b74
Mutai56 <mutaiemmanuel631@gmail.com>
2024-10-15 10:44:50 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const colorButton=document.getElementById('colorButton');
+
+colorButton.addEventListener('click',()=>{
+
+    let colorbutton;
+    colorButton.style.backgroundColor=colorbutton.style.backgroundColor==='blue'?'#4caf50':'blue';
+
+});
